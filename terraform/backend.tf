f112ccf884465d8f5417fce14a897d36f593faaf
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-07-15 20:15:41 +0300

    remove commented-out instructions from backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,10 @@
-# S3 backend for Terraform state
-# Steps to activate:
-#   1. terraform init                    (starts with local state)
-#   2. terraform apply                   (creates S3 bucket + DynamoDB table)
-#   3. Uncomment below
-#   4. terraform init -migrate-state     (migrates to S3)
-#
-
 terraform {
-  backend "s3" {
-    bucket         = "google-microservices-terraform-state"
-    key            = "microservices/terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
+   backend "s3" {
+     bucket         = "google-microservices-terraform-state"
+     key            = "microservices/terraform.tfstate"
+     region         = "eu-central-1"
+     encrypt        = true
+     dynamodb_table = "terraform-state-lock"
+   }
+ }
 
